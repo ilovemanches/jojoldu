1371b7df634f078d374a941ac76d506a72f99553
ilovemanches <ilovemanches@naver.com>
2020-10-11 12:33:03 +0900

    Unit Test Code for Controller Class

---
+++ src/test/java/jojoldu/web/HelloController.java
@@ -0,0 +1,11 @@
+package jojoldu.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/Hello")
+    public String hello(){
+        return "Hello";
+    }
+}
